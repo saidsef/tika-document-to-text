77db156f50d018ae69cd755ca380cbdac5329429
Said Sef <saidsef@gmail.com>
2020-05-02 08:41:21 +0100

    removed CSP RV entry

--- ui/app.js
+++ ui/app.js
@@ -39,7 +39,7 @@ app.use(helmet({
       imgSrc: ["'self'", 'data:'],
       objectSrc: ["'self'"],
       sandbox: ['allow-forms', 'allow-scripts'],
-      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "'unsafe-hashes'", 'code.jquery.com', 'cdnjs.cloudflare.com', 'code.responsivevoice.org'],
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "'unsafe-hashes'", 'code.jquery.com', 'cdnjs.cloudflare.com'],
       styleSrc: ["'self'", "'unsafe-inline'", 'stackpath.bootstrapcdn.com'],
       upgradeInsecureRequests: true,
     },
