6e34e466c7d508c0587414e9696aaefda68e65e2
Said Sef <saidsef@gmail.com>
2022-06-20 08:50:35 +0000

    Remove first char #101

--- ui/app.js
+++ ui/app.js
@@ -108,7 +108,7 @@ app.post('/', uploads.single('doc'), (req, res, next) => {
       });
       response.on("end", () => {
         res.render('index', {
-          text: Buffer.from(body, 'utf8').toString().replace(/<[^>]+>?/gmi, '').replace(/\n?\s{4,}/gmi, '\n\n').replace(/[^\x20-\x7E]/g, '').trim()
+          text: Buffer.from(body, 'utf8').toString().replace(/<[^>]+>?/gmi, '').replace(/\n?\s{4,}/gmi, '\n\n').trim().substring(1)
         });
       });
       response.on("error", (error) => {
